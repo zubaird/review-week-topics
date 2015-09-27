71eaa69f4536243af0024aa41084411bc0cbfc62
Elie Schoppik <eschoppik@gmail.com>
2015-09-27 15:53:51 -0700

    adding react example 2

---
+++ react/examples/react_tools/script.js
@@ -0,0 +1,11 @@
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+        <h1>
+          Hello World!
+        </h1>
+    );
+  }
+});
+
+React.render(<HelloWorld/>,document.body)
